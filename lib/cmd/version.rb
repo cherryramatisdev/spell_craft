98c97563ba267655f6cf37219ef47b4f95a14819
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-09-22 19:29:15 -0300

    fix(version): remove hardcoded version string
    Instead we're getting directly from the gemspec file

--- lib/cmd/version.rb
+++ lib/cmd/version.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 # typed: true
 
+require 'rubygems'
+
 module Cmd
   class Version < Dry::CLI::Command
     desc 'Print version'
 
     def call
-      puts '0.1.2'
+      puts Gem::Specification.load('spell_craft.gemspec').version
     end
   end
 end
