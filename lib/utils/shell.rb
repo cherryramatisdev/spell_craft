a3300e962ff1c8fcfa45ba3494ea9558245c6a5c
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-09-22 19:12:04 -0300

    fix(shell): improve the binary exists function

--- lib/utils/shell.rb
+++ lib/utils/shell.rb
@@ -8,6 +8,11 @@ module Utils
     sig { params(binary: String).returns(T::Boolean) }
     def self.binary_exists?(binary)
       File.executable?(File.join(ENV.fetch('PATH', '').split(':'), binary))
+      path_dirs = ENV.fetch('PATH', '').split(File::PATH_SEPARATOR)
+
+      !path_dirs.find do |dir|
+        File.exist?(File.join(dir, binary)) && !File.directory?(File.join(dir, binary))
+      end.nil?
     end
   end
 end
